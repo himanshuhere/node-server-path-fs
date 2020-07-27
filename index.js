d8a96024b5678a69cde505c58f118d3e6419057e
himanshuhere <himanshusoni3105@outlook.com>
2020-07-27 11:48:52 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 3100;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    console.log('Request for ' + req.url + ' by method ' + req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text.html');
-    res.end('<html><body><h1>Hey! I m connected.</h1></body></html>');
+    if (req.method == 'GET') {
+        var fileurl;
+        if (req.url == '/') fileurl = '/index.html';
+        else fileurl = req.url;
+
+        var filepath = path.resolve('./public' + fileurl);
+        const fileext = path.extname(filepath);
+        if (fileext == '.html') {
+            fs.exists(filepath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileurl +
+                        ' not found.</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filepath).pipe(res);
+
+            });
+
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + fileurl +
+                ' is not a HTML file</h1></body></html>');
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method +
+            ' not supported</h1></body></html>');
+    }
 });
 
 server.listen(port, hostname);
